ed192a37b05d41ef86d312802078bc4fadb3e798
Mihir Luthra <1999mihir.luthra@gmail.com>
2020-11-25 13:06:00 +0530

    add short description about the package

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,10 @@
+//! A fast lock free limited length free list.
+//!
+//! It uses bitmaps of type `usize` to keep track of the free list
+//! and hence free list has a size equal to number of bits in `usize`. If `usize` is `8` bytes then `64`
+//! and if `usize` is `4` bytes then `32`.
+
+
 mod dump;
 mod fbox;
 mod free_list;
