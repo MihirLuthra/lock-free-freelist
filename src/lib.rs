10fcdb4a496c197672755fd1ef595aaa1a906c96
Mihir Luthra <1999mihir.luthra@gmail.com>
2020-11-25 11:29:05 +0530

    pub use InitializableSmartPointer

--- src/lib.rs
+++ src/lib.rs
@@ -4,5 +4,5 @@ mod smart_pointer;
 mod fbox;
 
 pub use free_list::FreeList;
-pub use smart_pointer::SmartPointer;
+pub use smart_pointer::{SmartPointer, InitializableSmartPointer};
 pub use fbox::FBox;
