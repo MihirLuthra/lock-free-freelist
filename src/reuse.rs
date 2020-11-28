37cfbf49969e526a5b7186595d068c431d000ca8
Mihir Luthra <1999mihir.luthra@gmail.com>
2020-11-28 15:53:06 +0530

    add docs for Reuse

--- src/reuse.rs
+++ src/reuse.rs
@@ -1,6 +1,29 @@
 use super::{free_list::FreeList, smart_pointer::SmartPointer};
 use std::{mem::ManuallyDrop, ops::{Deref, DerefMut}};
 
+/// This is a wrapper around smart pointers so that
+/// when they are dropped, raw pointers contained in them can
+/// be put to free list and reused.
+///
+/// This can be produced by [FreeList::alloc](crate::FreeList::alloc)
+/// or [FreeList::recycle](crate::FreeList::recycle).
+///
+/// It implements Deref and DerefMut to access the wrapped smart pointer.
+///
+/// # Example
+///
+/// ```
+/// use lock_free_freelist::{FreeList, Reuse};
+///
+/// let free_list = FreeList::<Box<i32>>::new();
+///
+/// let reusable_box: Reuse<Box<i32>> = free_list.alloc(Box::new(5));
+///
+/// drop(reusable_box); // So that it can be recycled
+///
+/// let mut new_reusable_box: Reuse<Box<i32>> = free_list.recycle().unwrap();
+/// **new_reusable_box = 5;
+/// ```
 pub struct Reuse<'a, T: SmartPointer>
 where
     <T as Deref>::Target: Sized,
@@ -13,6 +36,7 @@ impl<'a, T: SmartPointer> Reuse<'a, T>
 where
     <T as Deref>::Target: Sized,
 {
+    /// Get a new [Reuse](crate::Reuse) instance.
     pub fn new<'b>(smart_pointer: T, free_list: &'b FreeList<T>) -> Reuse<'b, T> {
         Reuse {
             smart_pointer: ManuallyDrop::new(smart_pointer),
@@ -41,6 +65,9 @@ where
     }
 }
 
+/// When the instance of this type is dropped,
+/// an attempt is made to put the pointer to the contenst into free list
+/// and if free list is full, the contents are dropped.
 impl<'a, T: SmartPointer> Drop for Reuse<'a, T>
 where
     <T as Deref>::Target: Sized,
